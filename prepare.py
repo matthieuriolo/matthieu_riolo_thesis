bd488d47142d9e687273cbd700a341b95f3a2956
Matthieu Riolo <matthieu.riolo@qxmail.ch>
2022-10-12 22:44:42 +0200

    adding some dummy code for creating the prepare script

--- prepare.py
+++ prepare.py
@@ -30,4 +30,35 @@ def reset_weights(model):
             if var is not None:
                 var.assign(initializer(var.shape, var.dtype))
 
-            var.assign(initializer(var.shape, var.dtype)) #use the initializer
+            var.assign(initializer(var.shape, var.dtype)) # use the initializer
+
+
+
+
+# build inception from tensorflow application
+
+tf.keras.applications.inception_v3.InceptionV3(
+    include_top=True,
+    weights='imagenet',
+    input_tensor=None,
+    input_shape=None,
+    pooling=None,
+    classes=1000,
+    classifier_activation='softmax'
+)
+
+
+# https://tfhub.dev/google/bert_cased_L-24_H-1024_A-16/1
+
+model = tfhub.load('https://tfhub.dev/google/bert_cased_L-24_H-1024_A-16/1')
+
+
+# import tarfile
+# file = tarfile.open('bert.tar.gz')
+# file.extractall('./tmp')  
+# file.close()
+
+# from pathlib import Path
+# filename = Path('bert.tar.gz')
+# response = requests.get('https://tfhub.dev/google/bert_cased_L-24_H-1024_A-16/1?tf-hub-format=compressed')
+# filename.write_bytes(response.content)
