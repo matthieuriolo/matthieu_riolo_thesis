dddc0fbbff2343c8a6fd83df73ccc3e40ef991e2
Matthieu Riolo <matthieu.riolo@qxmail.ch>
2022-11-13 03:37:36 +0100

    adding some basic test case

--- run_tests.py
+++ run_tests.py
@@ -2,4 +2,17 @@ import config
 import utils
 
 
-for struct in structure_enumerate(config.CONTAINER_STRUCTURES):
+for structure in structure_enumerate(config.CONTAINER_STRUCTURES):
+    # ignore already executed test cases
+    if utils.structure_tested(structure):
+        continue
+    name = utils.structure_name(structure)
+
+    print(f'start test case {name}')
+    
+    test_case = utils.build_test(structure)
+    test_case.pre()
+    test_case.run()
+    test_case.post()
+    
+    print(f'end test case {name}')
