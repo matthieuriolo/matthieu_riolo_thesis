7b601eb07dfb9795932cbca298bcf6d84005e8a2
Matthieu Riolo <matthieu.riolo@qxmail.ch>
2023-03-15 20:29:47 +0100

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -108,25 +108,25 @@ def save_pci_speed(save_name, title, model_name, data_size, gpu_speed, with_pcie
 
 
 def build_pci_speed():
-    save_pci_speed('inception-g2-33-2', "Inception G3 - GPU 33% Daten 2%", 'inception', '2', '33', True)
-    save_pci_speed('inception-g2-66-2', "Inception G3 - GPU 66% Daten 2%", 'inception', '2', '66', True)
-    save_pci_speed('inception-g2-100-2', "Inception G3 - GPU 100% Daten 2%", 'inception', '2', '100', True)
+    save_pci_speed('inception-g2-33-2', "Inception G2 - GPU 33% Daten 2%", 'inception', '2', '33', True)
+    save_pci_speed('inception-g2-66-2', "Inception G2 - GPU 66% Daten 2%", 'inception', '2', '66', True)
+    save_pci_speed('inception-g2-100-2', "Inception G2 - GPU 100% Daten 2%", 'inception', '2', '100', True)
 
-    save_pci_speed('inception-g2-33-5', "Inception G3 - GPU 33% Daten 5%", 'inception', '5', '33')
-    save_pci_speed('inception-g2-66-5', "Inception G3 - GPU 66% Daten 5%", 'inception', '5', '66')
-    save_pci_speed('inception-g2-100-5', "Inception G3 - GPU 100% Daten 5%", 'inception', '5', '100')
+    save_pci_speed('inception-g2-33-5', "Inception G2 - GPU 33% Daten 5%", 'inception', '5', '33')
+    save_pci_speed('inception-g2-66-5', "Inception G2 - GPU 66% Daten 5%", 'inception', '5', '66')
+    save_pci_speed('inception-g2-100-5', "Inception G2 - GPU 100% Daten 5%", 'inception', '5', '100')
 
-    save_pci_speed('inception-g2-33-10', "Inception G3 - GPU 33% Daten 10%", 'inception', '10', '33')
-    save_pci_speed('inception-g2-66-10', "Inception G3 - GPU 66% Daten 10%", 'inception', '10', '66')
-    save_pci_speed('inception-g2-100-10', "Inception G3 - GPU 100% Daten 10%", 'inception', '10', '100')
+    save_pci_speed('inception-g2-33-10', "Inception G2 - GPU 33% Daten 10%", 'inception', '10', '33')
+    save_pci_speed('inception-g2-66-10', "Inception G2 - GPU 66% Daten 10%", 'inception', '10', '66')
+    save_pci_speed('inception-g2-100-10', "Inception G2 - GPU 100% Daten 10%", 'inception', '10', '100')
 
-    save_pci_speed('transformer-g2-33-2', "Transformer G3 - GPU 33% Daten 2%", 'transformer', '2', '33', True)
-    save_pci_speed('transformer-g2-66-2', "Transformer G3 - GPU 66% Daten 2%", 'transformer', '2', '66', True)
-    save_pci_speed('transformer-g2-100-2', "Transformer G3 - GPU 100% Daten 2%", 'transformer', '2', '100', True)
+    save_pci_speed('transformer-g2-33-2', "Transformer G2 - GPU 33% Daten 2%", 'transformer', '2', '33', True)
+    save_pci_speed('transformer-g2-66-2', "Transformer G2 - GPU 66% Daten 2%", 'transformer', '2', '66', True)
+    save_pci_speed('transformer-g2-100-2', "Transformer G2 - GPU 100% Daten 2%", 'transformer', '2', '100', True)
 
-    save_pci_speed('transformer-g2-33-5', "Transformer G3 - GPU 33% Daten 5%", 'transformer', '5', '33')
-    save_pci_speed('transformer-g2-66-5', "Transformer G3 - GPU 66% Daten 5%", 'transformer', '5', '66')
-    save_pci_speed('transformer-g2-100-5', "Transformer G3 - GPU 100% Daten 5%", 'transformer', '5', '100')
+    save_pci_speed('transformer-g2-33-5', "Transformer G2 - GPU 33% Daten 5%", 'transformer', '5', '33')
+    save_pci_speed('transformer-g2-66-5', "Transformer G2 - GPU 66% Daten 5%", 'transformer', '5', '66')
+    save_pci_speed('transformer-g2-100-5', "Transformer G2 - GPU 100% Daten 5%", 'transformer', '5', '100')
 
 
 # g3 - gpu speed
@@ -155,25 +155,25 @@ def save_gpu_speed(save_name, title, model_name, data_size, batch_size, with_pci
 
 
 def build_gpu_speed():
-    save_gpu_speed('inception-g3-32-2', "Inception G2 - Batch 32 Daten 2%", 'inception', '2', '32', True)
-    save_gpu_speed('inception-g3-64-2', "Inception G2 - Batch 64 Daten 2%", 'inception', '2', '64', True)
-    save_gpu_speed('inception-g3-128-2', "Inception G2 - Batch 128 Daten 2%", 'inception', '2', '128', True)
+    save_gpu_speed('inception-g3-32-2', "Inception G3 - Batch 32 Daten 2%", 'inception', '2', '32', True)
+    save_gpu_speed('inception-g3-64-2', "Inception G3 - Batch 64 Daten 2%", 'inception', '2', '64', True)
+    save_gpu_speed('inception-g3-128-2', "Inception G3 - Batch 128 Daten 2%", 'inception', '2', '128', True)
 
-    save_gpu_speed('inception-g3-32-5', "Inception G2 - Batch 32 Daten 5%", 'inception', '5', '32')
-    save_gpu_speed('inception-g3-64-5', "Inception G2 - Batch 64 Daten 5%", 'inception', '5', '64')
-    save_gpu_speed('inception-g3-128-5', "Inception G2 - Batch 128 Daten 5%", 'inception', '5', '128')
+    save_gpu_speed('inception-g3-32-5', "Inception G3 - Batch 32 Daten 5%", 'inception', '5', '32')
+    save_gpu_speed('inception-g3-64-5', "Inception G3 - Batch 64 Daten 5%", 'inception', '5', '64')
+    save_gpu_speed('inception-g3-128-5', "Inception G3 - Batch 128 Daten 5%", 'inception', '5', '128')
 
-    save_gpu_speed('inception-g3-32-10', "Inception G2 - Batch 32 Daten 10%", 'inception', '10', '32')
-    save_gpu_speed('inception-g3-64-10', "Inception G2 - Batch 64 Daten 10%", 'inception', '10', '64')
-    save_gpu_speed('inception-g3-128-10', "Inception G2 - Batch 128 Daten 10%", 'inception', '10', '128')
+    save_gpu_speed('inception-g3-32-10', "Inception G3 - Batch 32 Daten 10%", 'inception', '10', '32')
+    save_gpu_speed('inception-g3-64-10', "Inception G3 - Batch 64 Daten 10%", 'inception', '10', '64')
+    save_gpu_speed('inception-g3-128-10', "Inception G3 - Batch 128 Daten 10%", 'inception', '10', '128')
 
-    save_gpu_speed('transformer-g3-32-2', "Transformer G2 - Batch 32 Daten 2%", 'transformer', '2', '32', True)
-    save_gpu_speed('transformer-g3-64-2', "Transformer G2 - Batch 64 Daten 2%", 'transformer', '2', '64', True)
-    save_gpu_speed('transformer-g3-128-2', "Transformer G2 - Batch 128 Daten 2%", 'transformer', '2', '128', True)
+    save_gpu_speed('transformer-g3-32-2', "Transformer G3 - Batch 32 Daten 2%", 'transformer', '2', '32', True)
+    save_gpu_speed('transformer-g3-64-2', "Transformer G3 - Batch 64 Daten 2%", 'transformer', '2', '64', True)
+    save_gpu_speed('transformer-g3-128-2', "Transformer G3 - Batch 128 Daten 2%", 'transformer', '2', '128', True)
 
-    save_gpu_speed('transformer-g3-32-5', "Transformer G2 - Batch 32 Daten 5%", 'transformer', '5', '32')
-    save_gpu_speed('transformer-g3-64-5', "Transformer G2 - Batch 64 Daten 5%", 'transformer', '5', '64')
-    save_gpu_speed('transformer-g3-128-5', "Transformer G2 - Batch 128 Daten 5%", 'transformer', '5', '128')
+    save_gpu_speed('transformer-g3-32-5', "Transformer G3 - Batch 32 Daten 5%", 'transformer', '5', '32')
+    save_gpu_speed('transformer-g3-64-5', "Transformer G3 - Batch 64 Daten 5%", 'transformer', '5', '64')
+    save_gpu_speed('transformer-g3-128-5', "Transformer G3 - Batch 128 Daten 5%", 'transformer', '5', '128')
 
 
 # g1 - dataset speed
